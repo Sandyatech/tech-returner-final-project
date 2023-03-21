bfa6cd4cebb758da4c1ab8d80b1b9304a8e7fe5f
katiek78 <kermodekatie@gmail.com>
2023-03-21 16:49:33 +0000

    feat: Add top-level App layout, routes and error components

---
+++ src/components/Router.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from './MainLayout';
+import Home from './Home';
+import Favourites from './Favourites';
+import Current from './Current';
+import Forecast from './Forecast';
+import Historical from './Historical';
+import Error404 from './Error404';
+
+const Router : React.FC = () => <>
+    <Routes>
+        <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="/current" element={<Current />}></Route>
+            <Route path="/favourites" element={<Favourites />}></Route>
+            <Route path="/forecast" element={<Forecast />}></Route>
+            <Route path="/historical" element={<Historical />}></Route>
+            <Route path="*" element={<Error404 />}></Route>
+        </Route>
+    </Routes>
+</>;
+
+export default Router;

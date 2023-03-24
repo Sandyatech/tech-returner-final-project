30d9ec063fd259f9ee72a1e1d4e7016519971951
katiek78 <kermodekatie@gmail.com>
2023-03-24 15:06:02 +0000

    feat: Add new Health page

---
+++ src/API.ts
@@ -0,0 +1,9 @@
+export const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '3ef5c4e138msh91644e12beb4158p1e5cb6jsn1b6b43ab6590',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
+};
+
+export const WEATHERAPI_BASE_URL = "https://weatherapi-com.p.rapidapi.com/current.json";

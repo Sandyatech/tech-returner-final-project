e03d09d2c99c42be7321f08caad206c2bebb487a
katiek78 <kermodekatie@gmail.com>
2023-03-25 20:12:33 +0000

    feat: Modify routes

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './MainLayout';
 import Home from './Home';
-import FavouritesWeather from './Favourites';
-import Current from './Current';
+import Favourites from './Favourites';
+import CurrentWeather from './Current';
 import Forecast from './Forecast';
 import Historical from './Historical';
 import Health from './Health';
@@ -12,8 +12,8 @@ const Router : React.FC = () => <>
     <Routes>
         <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />}></Route>
-            <Route path="/current/:locationId" element={<Current />}></Route>
-            <Route path="/favourites" element={<FavouritesWeather />}></Route>
+            <Route path="/current" element={CurrentWeather("london")}></Route>
+            <Route path="/favourites" element={<Favourites />}></Route>
             <Route path="/forecast" element={<Forecast />}></Route>
             <Route path="/historical" element={<Historical />}></Route>
             <Route path="/health" element={<Health />}></Route>

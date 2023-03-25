4daf27c4a9be234cff94e6c3d40f03b1e9f88248
sandyatech <sandhya.mskgmail.com>
2023-03-25 14:51:41 +0000

    added header to the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,Link } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/ErrorFallback";
 import Router from "./components/Router";
@@ -9,6 +9,15 @@ function App() {
     return (
         <ErrorBoundary FallbackComponent={ErrorFallback}>
             <BrowserRouter>
+                <ul className="App-header">
+                    <li ><Link to='/' className="menu">Home</Link></li>
+                    <li ><Link to='/Current' className="menu">Current</Link></li>
+                    <li ><Link to='/Favorites' className="menu">FavouritesWeather</Link></li>
+                    <li ><Link to='/Forecast' className="menu">Forecast</Link></li>
+                    <li ><Link to='/Historical' className="menu">Historical</Link></li>
+                    <li ><Link to='/Health' className="menu">Health</Link></li>
+                </ul>
+                <hr />
                 <Router />
             </BrowserRouter>
         </ErrorBoundary>

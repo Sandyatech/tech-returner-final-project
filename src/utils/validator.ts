978566893178e2071c33b618fba1edee70d3173a
mikeyhw1 <70017382+mikeyhw1@users.noreply.github.com>
2023-03-26 22:56:12 +0100

    test: added unit test for utils & validator

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -2,7 +2,5 @@ export function dateValidator(input: -1 | 1, dateSubtract: number) {
     const max_dateSubtract = 7; // 7 days before
     const min_dateSubtract = 0; // today
     const adjusted_dateSubtract = dateSubtract + input;
-    if (adjusted_dateSubtract >= min_dateSubtract && adjusted_dateSubtract <= max_dateSubtract) {
-        return true;
-    }
+    return adjusted_dateSubtract >= min_dateSubtract && adjusted_dateSubtract <= max_dateSubtract;
 }

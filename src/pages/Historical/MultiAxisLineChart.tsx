1edeee6fc52f9d035ee6a2476811bda4dde7b000
mikeyhw1 <70017382+mikeyhw1@users.noreply.github.com>
2023-03-26 22:31:20 +0100

    feat: modular chart

---
+++ src/pages/Historical/MultiAxisLineChart.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    LineController,
+    LineElement,
+    PointElement,
+    LinearScale,
+    Title,
+    CategoryScale,
+    Tooltip,
+    Legend,
+} from "chart.js";
+import { Forecastday } from "../../components/interface_weather";
+
+interface MultiAxisLineChartProps {
+    labelArray: number[];
+    weatherData: Forecastday;
+}
+
+const MultiAxisLineChart: React.FC<MultiAxisLineChartProps> = ({ labelArray, weatherData }) => {
+    ChartJS.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, Tooltip, Legend);
+
+    const data = {
+        labels: labelArray,
+        datasets: [
+            {
+                label: "Temperature (C)",
+                data: weatherData?.hour?.map((data) => data.temp_c),
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+                yAxisID: "y",
+            },
+            {
+                label: "Humidity (%)",
+                data: weatherData?.hour?.map((data) => data.humidity),
+                borderColor: "rgb(53, 162, 235)",
+                backgroundColor: "rgba(53, 162, 235, 0.5)",
+                yAxisID: "y1",
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        interaction: {
+            mode: "index" as const,
+            intersect: false,
+        },
+        stacked: false,
+        scales: {
+            y: {
+                type: "linear" as const,
+                display: true,
+                position: "left" as const,
+            },
+            y1: {
+                type: "linear" as const,
+                display: true,
+                position: "right" as const,
+                grid: {
+                    drawOnChartArea: false,
+                },
+            },
+            x: {
+                title: {
+                    display: true,
+                    text: "Hour",
+                },
+            },
+        },
+    };
+
+    return <Line data={data} options={options} />;
+};
+
+export default MultiAxisLineChart;

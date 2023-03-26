34076186d1c72c7d8f2e105d6e8784bbd06286c8
Daniyal Ahmed Kiani <86118341+deekay11@users.noreply.github.com>
2023-03-26 10:43:56 +0100

    LocationForm: feat(LocationForm): add form component to search for location CurrentWeather: feat(CurrentWeather): add component to display current weather Forecast: feat(Forecast): add component to display weather forecast WeatherCard: feat(WeatherCard): add component to display weather data for a single day Types : feat(Types): added component to define the shape of the data returned by the weather API

---
+++ src/components/CurrentWeather.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { CurrentWeatherData } from "./types";
+
+interface CurrentWeatherProps {
+    currentWeather?: CurrentWeatherData;
+    locationName: string;
+    localTime: string;
+}
+
+
+const CurrentWeather: React.FC<CurrentWeatherProps> = ({ currentWeather, locationName, localTime }) => {
+    return (
+        <div>
+            <h2>Current weather in {locationName}</h2>
+            <p>Local time: {localTime}</p>
+            {currentWeather ? (
+                <div className="card">
+                    <p>Last updated: {currentWeather.last_updated}</p>
+                    <p>Temperature: {currentWeather.temp_c}°C</p>
+                    <p>Condition: {currentWeather.condition.text}</p>
+                    <img src={currentWeather.condition.icon} alt={currentWeather.condition.text} />
+                    <p>Wind speed: {currentWeather.wind_kph} km/h</p>
+                    <p>Humidity: {currentWeather.humidity}%</p>
+                    <p>Feels like: {currentWeather.feelslike_c}°C</p>
+                    <p>UV index: {currentWeather.uv}</p>
+                </div>
+            ) : (
+                <p>No current weather data available</p>
+            )}
+        </div>
+    );
+};
+
+export default CurrentWeather;

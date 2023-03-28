1c04c79e17ed3771ee066886eb3bfd1823879883
mikeyhw1 <70017382+mikeyhw1@users.noreply.github.com>
2023-03-28 15:34:54 +0100

    feat: forecast using global axios fetch, modify forecast to display current city, modify test in forecast

--- src/pages/Forecast/Forecast.tsx
+++ src/pages/Forecast/Forecast.tsx
@@ -1,23 +1,52 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+// import axios from "axios";
 import "../../App.css";
-import { Forecastday, CurrentWeatherData, Location } from "../../types/types_weather";
+import { Forecastday, CurrentWeatherData, Location, RootForecast } from "../../types/types_weather";
 import WeatherCard from "./WeatherCard";
 import CurrentWeather from "./CurrentWeather";
 import LocationForm from "./LocationForm";
-import { FORECAST_URL, options } from "../../services/api";
+// import { FORECAST_URL, options } from "../../services/api";
+import { fetchData } from "../../services/httpsServices";
+import { useCurrentLocation } from "../../hooks/CurrentLocationContext";
 
-function Forecast() {
-    const [forecast, setForecast] = useState<Forecastday[]>();
+const Forecast: React.FC = () => {
+    const [forecast, setForecast] = useState<Array<Forecastday>>();
     const [currentWeather, setCurrentWeather] = useState<CurrentWeatherData>();
     const [locationData, setLocationData] = useState<Location>();
 
+    const { currentLocation, setCurrentLocation } = useCurrentLocation();
+
     const handleSubmit = async (location: string) => {
-        const { data } = await axios.get(`${FORECAST_URL}?q=${location}&days=3`, options);
-        setForecast(data.forecast.forecastday);
-        setCurrentWeather(data.current);
-        setLocationData(data.location);
+        // const { data } = await axios.get(`${FORECAST_URL}?q=${location}&days=3`, options);
+        // setForecast(data.forecast.forecastday);
+        // setCurrentWeather(data.current);
+        // setLocationData(data.location);
+        setCurrentLocation(location);
     };
+
+    useEffect(() => {
+        if (currentLocation) {
+            const getData = async () => {
+                const params = {
+                    q: currentLocation,
+                    days: 3,
+                };
+                const response = (await fetchData({
+                    responseType: "RootForecast",
+                    params,
+                })) as RootForecast;
+
+                setForecast(response.forecast.forecastday);
+                setCurrentWeather(response.current);
+                setLocationData(response.location);
+            };
+            getData();
+
+            console.log("forecast");
+            console.log(forecast);
+        }
+        console.log("currentLocation: ", currentLocation);
+    }, [currentLocation]);
 
     return (
         <div className="App">
@@ -44,6 +73,6 @@ function Forecast() {
             )}
         </div>
     );
-}
+};
 
 export default Forecast;
--- src/pages/Forecast/WeatherCard.test.tsx
+++ src/pages/Forecast/WeatherCard.test.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import WeatherCard from './WeatherCard';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import WeatherCard from "./WeatherCard";
 
-describe('WeatherCard component', () => {
-    test('renders a message when no weather data is available', () => {
+describe("WeatherCard component", () => {
+    test("renders a message when no weather data is available", () => {
         const day = {
-            date: '2022-04-01',
-            hour: []
+            date: "2022-04-01",
+            hour: [],
+            date_epoch: 0,
+            day: {
+                maxtemp_c: 0,
+                maxtemp_f: 0,
+                mintemp_c: 0,
+                mintemp_f: 0,
+                avgtemp_c: 0,
+                avgtemp_f: 0,
+                maxwind_mph: 0,
+                maxwind_kph: 0,
+                totalprecip_mm: 0,
+                totalprecip_in: 0,
+                avgvis_km: 0,
+                avgvis_miles: 0,
+                avghumidity: 0,
+                condition: {
+                    text: "string",
+                    icon: "string",
+                    code: 0,
+                },
+                uv: 0,
+            },
+            astro: {
+                sunrise: "string",
+                sunset: "string",
+                moonrise: "string",
+                moonset: "string",
+                moon_phase: "string",
+                moon_illumination: "string",
+            },
         };
         render(<WeatherCard day={day} />);
-        expect(screen.getByText('No weather data available.')).toBeInTheDocument();
+        expect(screen.getByText("No weather data available.")).toBeInTheDocument();
     });
 });
